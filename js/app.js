30e03bb992cdf95ca89d0946c5b921858f2e6bed
nasitra <nasitra@users.noreply.github.com>
2023-10-14 23:21:00 +0900

    Make component of content

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-function main() { /* TODO */ }
+import { Content } from './core/content.js';
+import { WebContentComponent } from './infra/web-content-component.js';
+
+function main() {
+  const content = new Content();
+  content.component = new WebContentComponent(document.querySelector('.content'));
+}
 
 main();
