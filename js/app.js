e56052ffdef324beae344426eb265ca1af50fc7a
nasitra <nasitra@users.noreply.github.com>
2023-10-23 21:55:17 +0900

    Call 'start' method of 'content'

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ import { WebContentComponent } from './infra/web-content-component.js';
 function main() {
   const content = new Content();
   content.component = new WebContentComponent(document.querySelector('.content'));
+  content.start();
 }
 
 main();
