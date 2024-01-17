a17432dc2a426e8533a653cd09563fa0962f82a9
nasitra <nasitra@users.noreply.github.com>
2024-01-17 19:50:30 +0900

    Make stream-handler

--- cmd/streamer/main.go
+++ cmd/streamer/main.go
@@ -28,31 +28,132 @@ var upgrader = &websocket.Upgrader{
 	},
 }
 
-func server(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
+type connection struct {
+	conn    *websocket.Conn
+	handler *streamHandler
+	send    chan []byte
+}
+
+func (c *connection) read() {
+	defer func() {
+		c.handler.unregister <- c
+		c.conn.Close()
+	}()
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		c.handler.output <- msg
 	}
+}
+
+func (c *connection) write() {
+	defer c.conn.Close()
+	var t int
+	if *isBinary {
+		t = websocket.BinaryMessage
+	} else {
+		t = websocket.TextMessage
+	}
+	for msg := range c.send {
+		if err := c.conn.WriteMessage(t, msg); err != nil {
+			break
+		}
+	}
+}
+
+type streamHandler struct {
+	connections map[*connection]struct{}
+	register    chan *connection
+	unregister  chan *connection
+	output      chan []byte
+}
+
+func newStreamHandler() *streamHandler {
+	return &streamHandler{
+		connections: make(map[*connection]struct{}),
+		register:    make(chan *connection),
+		unregister:  make(chan *connection),
+		output:      make(chan []byte),
+	}
+}
+
+func (h *streamHandler) run() {
 	go func() {
-		defer c.Close()
-		for {
-			_, msg, err := c.ReadMessage()
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				break
+		if *isBinary {
+			in := bufio.NewReader(os.Stdin)
+			buf := make([]byte, upgrader.WriteBufferSize)
+			for {
+				n, err := in.Read(buf)
+				if n > 0 {
+					b := buf[0:n]
+					os.Stdout.Write(b)
+					for c := range h.connections {
+						select {
+						case c.send <- b:
+						default:
+							delete(h.connections, c)
+							close(c.send)
+						}
+					}
+				}
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+					break
+				}
 			}
+		} else {
+			scanner := bufio.NewScanner(os.Stdin)
+			for scanner.Scan() {
+				s := scanner.Text()
+				fmt.Println(s)
+				b := unsafe.Slice(unsafe.StringData(s), len(s))
+				for c := range h.connections {
+					select {
+					case c.send <- b:
+					default:
+						delete(h.connections, c)
+						close(c.send)
+					}
+				}
+			}
+		}
+	}()
+
+	for {
+		select {
+		case c := <-h.register:
+			h.connections[c] = struct{}{}
+		case c := <-h.unregister:
+			delete(h.connections, c)
+			close(c.send)
+		case msg := <-h.output:
 			if *isBinary {
 				os.Stdout.Write(msg)
 			} else {
 				fmt.Fprintln(os.Stdout, string(msg))
 			}
 		}
-	}()
+	}
+}
+
+func (h *streamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	c := &connection{conn: conn, handler: h, send: make(chan []byte, upgrader.WriteBufferSize)}
+	h.register <- c
+	go c.write()
+	go c.read()
 }
 
 func handleServer() {
-	http.HandleFunc("/ws", server)
+	handler := newStreamHandler()
+	go handler.run()
+	http.Handle("/ws", handler)
 	http.Handle("/", http.FileServer(http.Dir("./")))
 	http.ListenAndServe(":"+strconv.Itoa(*port), nil)
 }
